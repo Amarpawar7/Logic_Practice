f832ca535a9c8db42868eed51fefc7d0bab80306
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-30 19:55:36 +0530

    Add files via upload

---
+++ A4/2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int FactRev(int iNo){
+
+    int iCnt =0;
+    for (iCnt = (iNo/2); iCnt >= 1; iCnt--){
+        if((iNo%iCnt) == 0){
+            printf("%d",iCnt,"\t");
+        }
+    }
+}
+
+int main(){
+
+    int iValue = 0;
+
+    printf("Enter number :");
+    scanf("%d",&iValue);
+
+    FactRev(iValue);
+
+    return 0;
+}
